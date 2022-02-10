61f648c4c72c7ccca426bbe749df9172d3a5995d
Arielle <ariellecweber@gmail.com>
2022-02-10 01:06:12 -0600

    Part 4

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,35 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.launchcode.techjobs_oo.*;
+
+public class JobTest {
+
+    @Test
+    public void testSettingJobId(){
+        Job testJob1 = new Job();
+        Job testJob2 = new Job();
+        assertTrue((testJob2.getId() - testJob1.getId()) == 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job testJob3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertEquals("Product tester", testJob3.getName());
+        assertEquals("ACME", testJob3.getEmployer().getValue());
+        assertEquals("Desert", testJob3.getLocation().getValue());
+        assertEquals("Quality control", testJob3.getPositionType().getValue());
+        assertEquals("Persistence", testJob3.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality(){
+        Job testJob4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        Job testJob5 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
+        assertFalse(testJob4.equals(testJob5));
+    }
+}
